98108f19a0c0b527c00e265998c8745dc2bfc854
Varsha-S2005 <Varshapandian95@gmail.com>
2025-03-22 21:16:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,4 +67,4 @@ def calculate_emi_endpoint():
         return jsonify({"error": str(e)}), 400
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
