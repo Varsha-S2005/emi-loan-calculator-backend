4d9b86c71a57b8b94bb57776264ef51dade4f5f9
Varsha-S2005 <Varshapandian95@gmail.com>
2025-03-24 10:35:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -81,4 +81,4 @@ def health_check():
     return jsonify({"status": "UP"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='127.0.0.1', port=8080)
